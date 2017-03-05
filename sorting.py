530786e6b052635282a131c542140b9ec90b9e8c
ESQG <esqg@alumni.stanford.edu>
2017-03-04 23:31:13 -0800

    sorting: 20 mins

--- sorting.py
+++ sorting.py
@@ -7,7 +7,16 @@ def bubble_sort(lst):
         >>> bubble_sort([3, 5, 7, 2, 4, 1])
         [1, 2, 3, 4, 5, 7]
     """
-    pass
+    last_swap = len(lst) - 1
+
+    for j in xrange(len(lst)):
+        for i in xrange(last_swap):
+
+            if lst[i + 1] < lst[i]:
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
+                last_swap = i
+
+    return lst
 
 
 def merge_lists(list1, list2):
@@ -18,7 +27,23 @@ def merge_lists(list1, list2):
     [1, 3, 4, 7, 9, 11]
     """
 
-    pass
+    merged_list = []
+
+    while len(list1) > 0 or len(list2) > 0:
+
+        if len(list1) == 0:
+            return merged_list + list2
+
+        elif len(list2) == 0:
+            return merged_list + list1
+
+        elif list2[0] < list1[0]:
+            merged_list.append(list2.pop(0))
+
+        else:
+            merged_list.append(list1.pop(0))
+
+    return merged_list
 
 
 ##########ADVANCED##########
@@ -35,8 +60,15 @@ def merge_sort(lst):
     >>> merge_sort([6, 2, 3, 9, 0, 1])
     [0, 1, 2, 3, 6, 9]
     """
-    pass
 
+    if len(lst) < 2:
+        return lst
+
+    midpoint = len(lst) / 2
+    lst1 = merge_sort(lst[:midpoint])
+    lst2 = merge_sort(lst[midpoint:])
+
+    return merge_lists(lst1, lst2)
 
 
 
